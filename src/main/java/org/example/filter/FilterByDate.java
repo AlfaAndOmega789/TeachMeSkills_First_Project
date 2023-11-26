cc797533034e13496e0af87a513241c8b82436dc
Ivan <alfaandomega789@gmail.com>
2023-11-26 09:28:50 +0300

    init commit

--- src/main/java/org/example/filter/FilterByDate.java
+++ src/main/java/org/example/filter/FilterByDate.java
@@ -4,21 +4,27 @@ import org.example.list.txt.GenerateListTxt;
 import org.example.output.OutputReportInfo;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class FilterByDate {
 //    private static final String PATH_ARCHIVE = "src\\main\\java\\org\\example\\directory\\archive\\";
 //    private static final String NAME_REPORT_FILE = "FILE_REPORT_GENERAL.txt";
 
-//    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParseException {
 //        OutputReportInfo output = new OutputReportInfo();
 //        List<String> list = filterByDate(output.generateReportInfo(PATH_ARCHIVE, NAME_REPORT_FILE), "24112023");
 //
 //        for(String str : list){
 //            System.out.println(str);
 //        }
-//    }
+
+//        List<String> list = generateListAmountOfTime("25102023", "27112023");
+//        for(String str : list){
+//            System.out.println(str);
+//        }
+    }
 
     /**
      * Выводим те стркои в которых содержится переданная дата
@@ -34,5 +40,32 @@ public class FilterByDate {
             }
         }
         return newList;
+    }
+
+    /**
+     * Генерирует List дат в виде строк в указанный промежуток времени
+     * @param date1 - дата с(в виде строки)
+     * @param date2 - дата по(в виде строки)
+     * @return - List<String> в виде дат
+     * @throws ParseException
+     */
+    public static List<String> generateListAmountOfTime(String date1, String date2) throws ParseException {
+        List<String> listDate = new ArrayList<>();
+        SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyy");
+        Date date = sdf.parse(date1);
+        listDate.add(sdf.format(date));
+
+        while(true){
+            Calendar c = Calendar.getInstance();
+            c.setTime(date);
+            c.add(Calendar.DATE, 1);
+            date = c.getTime();
+            listDate.add(sdf.format(date));
+
+            if(sdf.format(date).equals(date2)){
+                break;
+            }
+        }
+        return listDate;
     }
 }
--- src/main/java/org/example/output/OutputReportInfo.java
+++ src/main/java/org/example/output/OutputReportInfo.java
@@ -3,28 +3,30 @@ package org.example.output;
 import org.example.filter.FilterByDate;
 
 import java.io.*;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class OutputReportInfo {
-//    private static final String PATH_ARCHIVE = "src\\main\\java\\org\\example\\directory\\archive\\";
-//    private static final String NAME_REPORT_FILE = "FILE_REPORT_GENERAL.txt";
-//    public static void main(String[] args) throws IOException {
+    private static final String PATH_ARCHIVE = "src\\main\\java\\org\\example\\directory\\archive\\";
+    private static final String NAME_REPORT_FILE = "FILE_REPORT_GENERAL.txt";
+    public static void main(String[] args) throws IOException, ParseException {
 //        outputReportInfo(PATH_ARCHIVE , NAME_REPORT_FILE);
-//    }
 
+        outputReportInfo(PATH_ARCHIVE, NAME_REPORT_FILE, generateReportAmountOfTheTime(PATH_ARCHIVE, NAME_REPORT_FILE, "26112023", "27112023"));
+
+    }
     /**
      * Выводим информацию из List в консоль
      * @param PATH_ARCHIVE - путь к файлу
      * @param NAME_REPORT_FILE - файл REPORT
      * @throws IOException
      */
-    public void outputReportInfo(String PATH_ARCHIVE, String NAME_REPORT_FILE, List<String> list) throws IOException {
+    public static void outputReportInfo(String PATH_ARCHIVE, String NAME_REPORT_FILE, List<String> list) throws IOException {
         for(String str : list){
             System.out.println(str);
         }
     }
-
     /**
      * Генерирует лист строк который содержится в файле REPORT
      * @param PATH_ARCHIVE
@@ -32,7 +34,7 @@ public class OutputReportInfo {
      * @return
      * @throws IOException
      */
-    public List<String> generateReportFullInfo(String PATH_ARCHIVE, String NAME_REPORT_FILE) throws IOException{
+    public static List<String> generateReportFullInfo(String PATH_ARCHIVE, String NAME_REPORT_FILE) throws IOException{
         List<String> list = new ArrayList<>();
         String reportAbsolutePath = PATH_ARCHIVE + NAME_REPORT_FILE;
         BufferedReader reader = new BufferedReader(new FileReader(reportAbsolutePath));
@@ -46,19 +48,44 @@ public class OutputReportInfo {
 
         return list;
     }
-
     /**
      * Генерирует лист строк за определнную дату из файла REPORT
      * @param PATH_ARCHIVE
      * @param NAME_REPORT_FILE
-     * @param str
+     * @param date
      * @return
      * @throws IOException
      */
-    public List<String> generateReportByDateInfo(String PATH_ARCHIVE, String NAME_REPORT_FILE, String str) throws IOException {
+    public List<String> generateReportByDateInfo(String PATH_ARCHIVE, String NAME_REPORT_FILE, String date) throws IOException {
         FilterByDate filter = new FilterByDate();
-        List<String> list = filter.filterByDate(generateReportFullInfo(PATH_ARCHIVE, NAME_REPORT_FILE),  str);
+        List<String> list = filter.filterByDate(generateReportFullInfo(PATH_ARCHIVE, NAME_REPORT_FILE),  date);
 
+        return list;
+    }
+
+    /**
+     * Метод генерирует List<String> из файла REPORT за определённый период времени
+     * @param PATH_ARCHIVE
+     * @param NAME_REPORT_FILE
+     * @param date1 - переданная первая дата в виде строки
+     * @param date2 - переданная вторая дата в виде строки
+     * @return
+     * @throws IOException
+     * @throws ParseException
+     */
+    public static List<String> generateReportAmountOfTheTime(String PATH_ARCHIVE, String NAME_REPORT_FILE, String date1, String date2) throws IOException, ParseException {
+        FilterByDate filter = new FilterByDate();
+        List<String> listFullInfo = generateReportFullInfo(PATH_ARCHIVE, NAME_REPORT_FILE);
+        List<String> listAmountOfTheTime = filter.generateListAmountOfTime(date1, date2);
+        List<String> list = new ArrayList<>();
+
+        for(String str1 : listFullInfo){
+            for(String str2 : listAmountOfTheTime){
+                if(str1.contains(str2)){
+                    list.add(str1);
+                }
+            }
+        }
         return list;
     }
 }
